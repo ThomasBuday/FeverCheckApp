d043fce30e263043d8862e573ba4cb0c41913f36
Thomas Buday <164018028+ThomasBuday@users.noreply.github.com>
2024-06-09 14:32:58 +0200

    Add project files.

---
+++ FeverCheckApp/Controllers/DoctorController.cs
@@ -0,0 +1,26 @@
+﻿using FeverCheckApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeverCheckApp.Controllers
+{
+    public class DoctorController : Controller
+    {
+
+        [HttpGet]                               // GET FeverCheck empty
+        public IActionResult FeverCheck()
+        {
+            return View();
+        }
+
+        [HttpPost]                              // POST FeverCheck with data
+        public IActionResult FeverCheck(double? temperature, string unit)
+        {
+            if (temperature.HasValue)
+            {
+                string result = TemperatureChecker.CheckTemperature(temperature.Value, unit);
+                ViewBag.Result = result;
+            }
+            return View();
+        }
+    }
+}
---
+++ FeverCheckApp/Models/TemperatureChecker.cs
@@ -0,0 +1,27 @@
+﻿namespace FeverCheckApp.Models
+{
+    public class TemperatureChecker
+    {
+        public static string CheckTemperature(double temperature, string unite)
+        {
+
+            if (unite == "F")           //Convert to Celsius
+            {
+                temperature = (temperature - 32) * 5 / 9;
+            }
+
+            if (temperature >= 38.0)    //In case of fever
+            {
+                return "You have a fever";
+            }
+            else if (temperature <= 35) //In case of Hypothermia
+            {
+                return "You have hypothermia";
+            }
+            else                        //In case of nothing...
+            {
+                return "You are ok";
+            }
+        }
+    }
+}
